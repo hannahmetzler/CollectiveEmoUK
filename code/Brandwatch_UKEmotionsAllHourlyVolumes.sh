4533ae01be91c5f18948edc38143633c8bdbf43e
Hannah Metzler <metzler@csh.ac.at>
2023-03-30 13:33:08 +0200

    uncommented

--- code/Brandwatch_UKEmotionsAllHourlyVolumes.sh
+++ code/Brandwatch_UKEmotionsAllHourlyVolumes.sh
@@ -7,7 +7,6 @@ sh Brandwatch-DownloadVolumeHourly.sh 2001637764
 mv volume-2001637764-$today.csv UK_sadness_2019-01-01_$today.csv
 sleep 40
 
-<<comment
 sh Brandwatch-DownloadVolumeHourly.sh 2001333247 
 mv volume-2001333247-$today.csv UK_total_2019-01-01_$today.csv
 sleep 40
@@ -20,7 +19,6 @@ sh Brandwatch-DownloadVolumeHourly.sh 2001637850
 mv volume-2001637850-$today.csv UK_posemo_2019-01-01_$today.csv
 sleep 40
 
-#change this one for Germany
 sh Brandwatch-DownloadVolumeHourly.sh 2001637729
 mv volume-2001637729-$today.csv UK_anxiety_2019-01-01_$today.csv
 sleep 40
@@ -29,6 +27,8 @@ sh Brandwatch-DownloadVolumeHourly.sh 2001616857
 mv volume-2001616857-$today.csv UK_anger_2019-01-01_$today.csv
 sleep 40
 
+
+<<comment
 comment
 
 
